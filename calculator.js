27ddb990570b10f251b63117ab6f55f20a349b48
Cameron Maloney <mac6@129mac18.valleychristian.net>
2017-01-12 10:20:34 -0800

    final calculator v 1.0

--- calculator.js
+++ calculator.js
@@ -2,20 +2,20 @@
 
 // Change Display
 function d(val) {
-	document.getElementById("d") .value = val;
+	document.getElementById("d").value = val;
 }
 
 // Type numbers and operators
-function v(val)	 {
-	document.getElementById("v") .value += val;
+function v(val) {
+	document.getElementById("d").value += val;
 }
-	
+
 // Evaluate the equation
-function e()	{
+function e() {
 	try {
-			d(eval(document.getElementById("d") .value));
+		d(eval(document.getElementById("d").value));
 	}
-	catch(err)	 {
-			d("Error");
+	catch(err) {
+		d("Error");
 	}
 }
